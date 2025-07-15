cd73051e18f7f4442d7816138d6c4854351f8ceb
sbc605 <sbc605@naver.com>
2025-07-15 23:32:44 +0900

    보기 퍼즐 4개 마스크 처리 구현
    [추가해야할 것]
    1.이미지 위치 다르게 하기
    2.보기 4개 균일 간격으로 위치 잡기

--- Assets/KimSua/02. Scripts/PuzzleExample.cs
+++ Assets/KimSua/02. Scripts/PuzzleExample.cs
@@ -5,19 +5,18 @@ using UnityEngine.UI;
 
 public class PuzzleExample : MonoBehaviour
 {
-    public Image bgImage; // 원본 이미지
+    public GameObject[] bgImagePrefab; // 명화 프리팹
     public GameObject[] puzzlePrefab; // 퍼즐 프리팹
     [SerializeField] private Transform puzzleParent; // 보기 4개 배치할 부모
-    [SerializeField] private RectTransform puzzleArea; // 보기 4개 배치할 영역
 
     private int gridX = 5;
     private int gridY = 3;
 
     private int correctIndex;
-    private List<Sprite> wrongSprites = new List<Sprite>();
 
-    private List<(int x, int y)> selectedPos = new List<(int x, int y)>();
-    private GameObject[] selectedPrefabs;
+    private List<(int x, int y)> selectedPos = new List<(int x, int y)>(); // 4개(정답1 + 오답3)
+
+    // --------------------------------------------------------------------------------------------------------
 
     void Start()
     {
@@ -27,14 +26,13 @@ public class PuzzleExample : MonoBehaviour
     void PuzzleOptions()
     {
         SelectRandomPos();
-        //CorrectPuzzle();
-        //WrongPuzzles();
         ExPuzzleInst();
-        CreateHole();
     }
 
+    // --------------------------------------------------------------------------------------------------------
+
     /// <summary>
-    ///  이미지 랜덤 4개 위치 선택
+    ///  (정답 포함)랜덤 4개 위치 선택
     /// </summary>    
 
     void SelectRandomPos()
@@ -77,198 +75,101 @@ public class PuzzleExample : MonoBehaviour
     }
 
 
-    ///// <summary>
-    /////  정답 조각 랜덤 위치 & Sprite 생성
-    ///// </summary> 
-    //public void CorrectPuzzle()
-    //{
-    //    int answerX = Random.Range(0, gridX);
-    //    int answerY = Random.Range(0, gridY);
+    /// <summary>
+    ///  정답 조각 랜덤 위치 & Sprite 생성
+    /// </summary> 
+    Vector2 CorrectPuzzle()
+    {
+        var pos = selectedPos[correctIndex];
+        return new Vector2(pos.x, pos.y);
+    }
 
-    //    correctSprite = CutSprite(answerX, answerY);
-    //}
+    /// <summary>
+    ///  오답 조각 랜덤 위치 크롭
+    /// </summary> 
+    List<Vector2> WrongPuzzles()
+    {
+        List<Vector2> wrongs = new List<Vector2>();
 
-    ///// <summary>
-    /////  오답 조각 랜덤 위치 크롭
-    ///// </summary> 
-    //void WrongPuzzles()
-    //{
-    //    wrongSprites.Clear();
+        for (int i = 0; i < selectedPos.Count; i++)
+        {
+            if (i == correctIndex) continue;
+            wrongs.Add(new Vector2(selectedPos[i].x, selectedPos[i].y));
+        }
 
-    //    while (wrongSprites.Count < 3)
-    //    {
-    //        int x = Random.Range(0, gridX);
-    //        int y = Random.Range(0, gridY);
-
-    //        Sprite wrong = CutSprite(x, y);
-    //        wrongSprites.Add(wrong);
-    //    }
-    //}
+        return wrongs;
+    }
 
     /// <summary>
     ///  보기 퍼즐 4개 생성
     /// </summary>
     void ExPuzzleInst()
     {
-        // 이전 퍼즐 제거
+        // 퍼즐 초기화
         foreach (Transform child in puzzleParent)
         {
             Destroy(child.gameObject);
         }
 
-        // 15개 중 4개 랜덤 선택
-        selectedPrefabs = SelectRandomPrefabs();
+        // 랜덤 명화 1개 선택
+        GameObject bgRandom = bgImagePrefab[Random.Range(0, bgImagePrefab.Length)];
+
+        float fullWidth = 650f;
+        float fullHeight = 800f;
+        float cellWidth = fullWidth / gridX;
+        float cellHeight = fullHeight / gridY;
+
+        // 퍼즐 모양 15개 중 4개 랜덤 선택
+        GameObject[] selectedPrefabs = SelectRandomPrefabs();
+
+        Vector2 correctPos = CorrectPuzzle(); // 정답 위치
+        List<Vector2> wrongPosList = WrongPuzzles(); // 오답 위치들
 
         // 4개 퍼즐 생성
         for (int i = 0; i < 4; i++)
         {
-            GameObject prefab = selectedPrefabs[i]; // 선택된 4개 퍼즐
-            GameObject puzzleObj = Instantiate(prefab, puzzleParent);
-
-            // UI Image 컴포넌트 찾기
-            Image img = puzzleObj.GetComponent<Image>();
-            if (img == null)
+            Vector2 pos;
+            if (i == correctIndex)
             {
-                img = puzzleObj.GetComponentInChildren<Image>();
+                pos = correctPos;
             }
-
             else
             {
-                var pos = selectedPos[i];
-
-                // 프리팹 모양대로 배경 이미지 자르기
-                Sprite pieceSr = CutSprite(pos.x, pos.y, prefab);
-                img.sprite = pieceSr;
-                img.color = Color.white;
-            }
-        }
-
-        // 퍼즐 조각들 일정 간격으로 배치
-        ArrangePuzzle();
-    }
-
-    /// <summary>
-    /// 퍼즐 조각 일정 간격 배치
-    /// </summary>
-    void ArrangePuzzle()
-    {
-        float spacingX = puzzleArea.rect.width / 2;
-        float spacingY = puzzleArea.rect.height / 2;
-
-        for (int i = 0; i < puzzleParent.childCount && i < 4; i++)
-        {
-            Transform child = puzzleParent.GetChild(i);
-            RectTransform childRect = child.GetComponent<RectTransform>();
-            if (childRect == null) continue;
-
-            float x = (i % 2) * spacingX - spacingX / 2;
-            float y = spacingY / 2 - (i / 2) * spacingY;
-
-            childRect.anchoredPosition = new Vector2(x, y);
-        }
-    }
-
-
-    /// <summary>
-    ///  퍼즐 조각 영역만큼 이미지 잘라서 사용
-    /// </summary>
-
-    Sprite CutSprite(int tileX, int tileY, GameObject prefab)
-    {
-        Texture2D bgTex = bgImage.sprite.texture;
-
-        int cellWidth = bgImage.sprite.texture.width / gridX;
-        int cellHeight = bgImage.sprite.texture.height / gridY;
-
-        int startX = tileX * cellWidth;
-        int startY = tileY * cellHeight;
-
-        // 프리팹 스프라이트 마스크로 사용
-        Image prefabImg = prefab.GetComponent<Image>();
-
-        Texture2D newTex = new Texture2D(cellWidth, cellHeight);
-
-        if (prefabImg != null && prefabImg.sprite != null)
-        {
-            Texture2D maskTex = prefabImg.sprite.texture;
-
-            for (int x = 0; x < cellWidth; x++)
-            {
-                for (int y = 0; y < cellHeight; y++)
+                if (i < correctIndex)
                 {
-                    // 마스크 위치 계산, 알파값 확인
-                    int maskX = x * maskTex.width / cellWidth;
-                    int maskY = y * maskTex.height / cellHeight;
-
-                    // 마스크 영역 안에 배경 이미지 복사
-                    if (maskTex.GetPixel(maskX, maskY).a > 0.1f)
-                    {
-                        Color bgColor = bgTex.GetPixel(startX + x, startY + y);
-                        newTex.SetPixel(x, y, bgColor);
-                    }
+                    pos = wrongPosList[i];
+                }
+                else
+                {
+                    pos = wrongPosList[i - 1];
                 }
             }
-        }
 
-        /* 원본 배경 이미지에서 해당 영역 복사(기본 사각형 모양)
-        for (int x = 0; x < cellWidth; x++)
-        {
-            for (int y = 0; y < cellHeight; y++)
+            // 퍼즐 외형 프리팹 (Mask용)
+            GameObject puzzleObj = Instantiate(selectedPrefabs[i], puzzleParent);
+            Image puzzleImg = puzzleObj.GetComponent<Image>();
+            puzzleImg.SetNativeSize();
+
+            GameObject childImg = Instantiate(bgRandom, puzzleObj.transform);
+            Image img = childImg.GetComponent<Image>();
+            RectTransform bgRect = img.GetComponent<RectTransform>();
+
+            if (img != null)
             {
-                Color color = originalTex.GetPixel(startX + x, startY + y);
-                newTex.SetPixel(x, y, color);
-            }
-        } */
-        newTex.Apply();
-        return Sprite.Create(newTex, new Rect(0, 0, cellWidth, cellHeight), new Vector2(0.5f, 0.5f));
-    }
+                img.raycastTarget = false;
 
-    void CreateHole()
-    {
-        GameObject correctPrefab = selectedPrefabs[correctIndex];
-        var correctPos = selectedPos[correctIndex];
+                // 크기 고정 설정
+                bgRect.anchorMin = new Vector2(0.5f, 0.5f);
+                bgRect.anchorMax = new Vector2(0.5f, 0.5f);
+                bgRect.pivot = new Vector2(0.5f, 0.5f);
+                bgRect.sizeDelta = new Vector2(fullWidth, fullHeight);
 
-        Texture2D bgTex = bgImage.sprite.texture;
-        Texture2D newBgTex = new Texture2D(bgTex.width, bgTex.height);
+                // 명화 위치 이동
+                //float offsetX = -(cellWidth * (gridX - 1) / 2f) + pos.x * cellWidth;
+                //float offsetY = (cellHeight * (gridY - 1) / 2f) + pos.y * cellHeight;
+                //bgRect.anchoredPosition = new Vector2(offsetX, offsetY);
 
-        // 원본 배경 이미지 복사
-        for (int x = 0; x < bgTex.width; x++)
-        {
-            for (int y = 0; y < bgTex.height; y++)
-            {
-                newBgTex.SetPixel(x, y, bgTex.GetPixel(x,y));
             }
         }
-
-        // 정답 위치에 구멍 생성
-        int cellWidth = bgTex.width / gridX;
-        int cellHeight = bgTex.height / gridY;
-
-        int startX = correctPos.x * cellWidth;
-        int startY = correctPos.y * cellHeight;
-
-        Image prefabImg = correctPrefab.GetComponent<Image>();
-        Texture2D maskTex = prefabImg.sprite.texture;
-
-        for (int x = 0; x < cellWidth; x++)
-        {
-            for (int y = 0; y < cellHeight; y++)
-            {
-                // 마스크 위치 계산, 알파값 확인
-                int maskX = x * maskTex.width / cellWidth;
-                int maskY = y * maskTex.height / cellHeight;
-
-                // 검정색 설정
-                if (maskTex.GetPixel(maskX, maskY).a > 0.1f)
-                {
-                    newBgTex.SetPixel(startX + x, startY + y, Color.black);
-                }
-            }
-        }
-
-        newBgTex.Apply();
-
-        Sprite newBgSr = Sprite.Create(newBgTex, new Rect(0, 0, newBgTex.width, newBgTex.height), new Vector2(0.5f, 0.5f));
-        bgImage.sprite = newBgSr;
     }
 }
